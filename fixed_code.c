4a354927cdb71a84350d9df9645df1ebc6cde75a
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-05-29 14:16:04 +0200

    Implement add_node_end function to add node at end

---
+++ fixed_code.c
@@ -0,0 +1,19 @@
+/* Corrected Function - add_node_end */
+list_t *add_node_end(list_t *head, const int n)
+{
+    list_t *new_node;
+    list_t *current;
+
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->n    = n;
+    new_node->next = NULL;
+    if (!head)
+        return (new_node);
+    current = head;
+    while (current->next != NULL)
+        current = current->next;
+    current->next = new_node;
+    return (head);
+}
